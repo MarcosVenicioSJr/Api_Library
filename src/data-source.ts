8bdd1662700deb1a96300a2752a9a066d6da6224
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-03-01 19:58:54 -0300

    Criando e configurando data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource } from "typeorm";
 
+const port = process.env.DB_PORT as number | undefined
+
 const AppDataSource = new DataSource({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "test",
-        password: "test",
-        database: "test",
+        host: process.env.DB_HOST,
+        port: port,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         synchronize: true,
         logging: true,
         subscribers: [],
