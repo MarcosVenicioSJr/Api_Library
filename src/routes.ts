0562d4e9f44cb03bd739f1bbbe7ed7ff63d83b5d
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-03-11 08:31:52 -0300

    Mapeamento de rotas ajeitado. Item seguindo para Homologação

--- src/routes.ts
+++ src/routes.ts
@@ -6,16 +6,17 @@ import AdminController from "./controllers/AdminController";
 const routes = Router();
 
 routes.get('/book', BookController.getBooksByTittle)
+routes.post('/book', AdminController.createBook)
+routes.delete('/book', AdminController.deleteBook)
+
 routes.get('/student', StudentController.getStudentById)
 routes.get('/getAllStudent', StudentController.getAllBooks)
 routes.get('/getAllStudent', StudentController.getAllStudents)
-
-routes.post('/book', AdminController.createBook)
-routes.post('/admin', AdminController.createAdmin)
 routes.post('/student/rented', StudentController.rentedBooks)
 routes.post('/student/returnBook', StudentController.returnBook)
 
-routes.delete('/book', AdminController.deleteBook)
 
+routes.post('/admin', AdminController.createAdmin)
+routes.delete('/admin', AdminController.deleteAdmin)
 
 export default routes;
